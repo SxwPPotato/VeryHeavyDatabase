cadc2f6c8e94a1430798b631f3f795e1d23b1c89
Vitalya <vitalyabes.vb@gmail.com>
2024-05-25 09:41:25 +0300

    Initial commit

---
+++ VeryHeavyDatabase.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+
+class VeryHeavyDatabase {
+
+public:
+
+     std::string GetData(const std::string& key)  {
+
+        return "Very big data string" + key;
+
+    }
+
+
+};
+
+class OneShotDB : public VeryHeavyDatabase {
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots_): real_db(real_object), shots(shots_) {
+       
+    };
+
+    
+    std::string GetData(const std::string& key)  {
+
+   
+        if (shots != 0) {
+            shots--;
+
+            return real_db->GetData(key);
+            
+        }
+        else return "eror";
+
+    }
+private:
+    std::unique_ptr<VeryHeavyDatabase> real_db;
+    size_t shots;
+};
+
+int main()
+{
+    auto real_db = VeryHeavyDatabase();
+
+
+
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+
+}
